d4575c7884b5496f01f8a8ebbe2c2397a2407b8e
kate102 <kate.morris.new@gmail.com>
2019-03-20 16:53:45 +0000

    Completed single note view

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,8 +1,7 @@
-console.log("hello1");
 (function(exports) {
-  console.log("hello2");
+
   function HTMLContainsDefaultNote() {
-    console.log("hello3");
+
     // create double of noteList called "noteListDouble"
     function noteDouble() {
       this.text = "Favourite drink: seltzer"
@@ -18,14 +17,10 @@ console.log("hello1");
         this.notesArray.push(note);
       }
     }
-
     var noteListDouble = new noteListDouble()
-
     var noteController = new NoteController(noteListDouble)
-    console.log(noteController.noteListView.showHTML());
     assertionToCheck = (noteController.noteListView.showHTML() === "<ul><li><div>Favourite drink: seltzer</div></li></ul>")
     assert.isTrue(assertionToCheck)
-
   }
 
   HTMLContainsDefaultNote()
---
+++ spec/single-note-view-spec.js
@@ -0,0 +1,24 @@
+// Code is wrapped in the module pattern.
+// Code uses the constructor and prototype pattern to define a single note view object that can be instantiated.
+// The single note view constructor takes a note model as a parameter.
+// The single note view has a method that, when called, returns a string of HTML that represents the note model.
+// For example: <div>Favourite drink: seltzer</div>.
+(function(exports) {
+
+  function SingleNoteViewTest() {
+    // create double of noteList called "noteListDouble"
+    function noteDouble() {
+      this.text = "Favourite drink: seltzer"
+    }
+
+    var noteDouble = new noteDouble()
+    var testStr = "<div>Favourite drink: seltzer</div>"
+    var singleNoteView = new SingleNoteView(noteDouble)
+
+    assertionToCheck = (singleNoteView.returnHTML() === testStr)
+    assert.isTrue(assertionToCheck)
+
+
+  }
+  SingleNoteViewTest()
+})(this)
